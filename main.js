b10412665c17b2c4751354e64ac1c59ffa982d7a
lassespilling <lassespilling@gmail.com>
2026-05-29 12:22:13 +0200

    fixes

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-// Entry point
+import { showToast } from 'https://unpkg.com/@aksell/ui/src/components/Toast/toast.js';
+import 'https://unpkg.com/@aksell/ui/src/components/PillArrowBtn/standalone/PillArrowBtn.standalone.js';
+
+document.querySelectorAll('[data-toast]').forEach((btn) => {
+  btn.addEventListener('click', () => {
+    const variant = btn.dataset.toast;
+    const message = btn.dataset.toastMsg ?? 'Toast';
+    showToast(message, { variant });
+  });
+});
